cde024175d4b7d90036d4881c0e1f9e2531fa184
Faiz4211 <faizk2631@gmail.com>
2023-02-06 19:53:41 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState();
+  const [input, setInput] = useState('');
+
+  const apiKey = '324a51544441d6b6e0dfacc3239075b5';
+
+  const searchWeather = async (cityName) => {
+    if (!cityName) return
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+    await axios.get(apiURL)
+      .then((res) => {
+        console.log(res.data)
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const handleSearch = () => {
+    if (!input) {
+      alert('Please enter city name')
+    }
+    searchWeather(input);
+    setInput('');
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="col-md-12">
+        <div className="weather">
+          <h1 className='heading'>Weather App</h1>
+          <div className="d-grid gap-3 col-4 mt-4">
+            <input type="text" className='form-control' value={input} onChange={handleChange} />
+            <button onClick={handleSearch} className='btn btn-primary' type='button'>Search</button>
+          </div>
+        </div>
+
+        <div className="col-md-12 text-center mt-5">
+          <div className="shadow rounded weatherResult">
+            <img className='icon' src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" alt="weather-icon" />
+            {data ?
+              (<div>
+                <h5 className='city'>{data?.name}</h5>
+                <h6 className="temp">{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+              </div>)
+              : (<h1>Nothing to show, Please search for city name!</h1>)}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
