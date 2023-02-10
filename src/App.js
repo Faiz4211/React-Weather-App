7fb3b9cf58ac6fab7047082365c0b16fe90d925b
Faiz4211 <faizk2631@gmail.com>
2023-02-10 13:18:21 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import axios from 'axios';
 
 function App() {
